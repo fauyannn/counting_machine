1d38d7e825f9e31a8cc34a0664647905367eb4d7
Fauyan <fauyannn@gmail.com>
2019-09-30 17:59:18 +0000

    fix api get counting machine

--- counting_machine/counting_machine/doctype/counting_machine/counting_machine.py
+++ counting_machine/counting_machine/doctype/counting_machine/counting_machine.py
@@ -21,20 +21,23 @@ def get_cm(rf_id='',mesin_id=''):
 	
 	job_card = frappe.db.get_value('Job Card', filters, ['job_started','for_quantity','name'])
 
-	job_started = 1
-	if (int(job_card[0]) > 0):
-		job_started = 0
+	if(job_card):
+		job_started = 1
+		if (int(job_card[0]) > 0):
+			job_started = 0
 
-	job_id = job_card[2]
-	frappe.db.set_value("Job Card", job_id, "job_started", job_started)
- 
-	return {
-		'status' : job_card[0],
-		'employee_name' : data_employee['employee_name'],
-		'target' : (job_card[1]),
-		'mesin_id' : mesin_id,
-		'job_id' : job_id
-	}
+		job_id = job_card[2]
+		frappe.db.set_value("Job Card", job_id, "job_started", job_started)
+	
+		return {
+			'status' : job_card[0],
+			'employee_name' : data_employee['employee_name'],
+			'target' : (job_card[1]),
+			'mesin_id' : mesin_id,
+			'job_id' : job_id
+		}
+	else:
+		return {'status':'Job Card not available'}
 
 @frappe.whitelist()
 def set_actual(job_id,actual):
@@ -52,5 +55,8 @@ def get_employee(rf_id):
 	filters = {
 		"rf_id" : ["=",rf_id]
 	}
-	employee,employee_name = frappe.db.get_value('Employee',filters,['employee','employee_name'])
-	return {'employee_id':employee, 'employee_name':employee_name}
+	data = frappe.db.get_value('Employee',filters,['employee','employee_name'])
+	if(data):
+		return {'employee_id':data[0], 'employee_name':data[1]}
+	else:
+		return {'employee_id':'0', 'employee_name':'0'}
