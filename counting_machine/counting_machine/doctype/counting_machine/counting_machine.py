98deb65e4142ff7e8d4a34fdc6ba26b602a847de
Fauyan <fauyannn@gmail.com>
2019-10-04 08:56:32 +0000

    API Start Job and set actual

--- counting_machine/counting_machine/doctype/counting_machine/counting_machine.py
+++ counting_machine/counting_machine/doctype/counting_machine/counting_machine.py
@@ -25,25 +25,59 @@ def get_cm(rf_id='',mesin_id=''):
 		}
 	
 	job_card = frappe.db.get_value('Job Card', filters, ['job_started','for_quantity','name'])
-
+	
 	if job_card:
 		job_id = job_card[2]
 		data = frappe.get_doc('Job Card', job_id)
-		
-		job_started = 1
+		# return data
+		_count = 0	
+		for dt in data.time_logs:
+			_count = dt.idx
+		# return dt
+		_now = frappe.utils.now()
+		_job_started = 1
 		if (int(job_card[0]) > 0):
-			job_started = 0
+			_job_started = 0
+			newtime = {
+				"from_time": dt.from_time,
+				"docstatus": dt.docstatus,
+				"doctype": dt.doctype,
+				"name": dt.name,
+				"owner": dt.owner,
+				"completed_qty": dt.completed_qty,
+				"parent": job_id,
+				"parentfield": dt.parentfield,
+				"parenttype": dt.parenttype,
+				"idx": _count,
+				"actual": dt.actual,
+				"time_in_mins": dt.time_in_mins,
+				"modified_by": dt.modified_by,
+				"modified": _now,
+				"creation": dt.creation,
+				"not_good": dt.not_good,
+				"to_time": _now
+			}
+			# data.modified = _now
+			data.__unsaved = 1
+			data.job_started = 0
+			data.time_logs[(_count-1)].parent = dt.parent
+			data.time_logs[(_count-1)].idx = dt.idx
+			data.time_logs[(_count-1)].name = dt.name
+			data.time_logs[(_count-1)].from_time = dt.from_time
+			data.time_logs[(_count-1)].to_time = frappe.utils.now()
+			data.time_logs[(_count-1)].actual = dt.actual
+			data.time_logs[(_count-1)].__unsaved = 1
+			# data.time_logs[(_count-1)].time_in_mins = dt.time_in_mins
 
-		else:		
-
-			_count = 0	
-			for dt in data.time_logs:
-				_count = dt.idx
+			# return data
+		else:
+			
 			new_idx = _count+1
 			newtime = {
+				"from_time": _now,
 				"docstatus": 0,
 				"doctype": "Job Card Time Log",
-				"name": "New Job Card Time Log 1",
+				"name": "New Job Card Time Log {}".format(new_idx),
 				"__islocal": 1,
 				"__unsaved": 1,
 				"owner": "Administrator",
@@ -51,26 +85,29 @@ def get_cm(rf_id='',mesin_id=''):
 				"parent": job_id,
 				"parentfield": "time_logs",
 				"parenttype": "Job Card",
-				"idx": new_idx,
-				"from_time": frappe.utils.now() 
+				"idx": new_idx 
 				}
 			
-			data.started_time = frappe.utils.now()
+			data.started_time = _now
+			# data.modified = _now
 			data.__unsaved = 1
 			data.job_started = 1
-			data.total_completed_qty = 10
+			# data.total_completed_qty = 10
 			data.append("time_logs", newtime)
-			# return data
-			data.save(
-				ignore_permissions=True, # ignore write permissions during insert
-    			ignore_version=True # do not create a version record
-			)
+			
+		data.save(
+			ignore_permissions=True, # ignore write permissions during insert
+			ignore_version=True # do not create a version record
+		)
 
-			return data
+		# return data
 
 		# job_card = frappe.db.set_value("Job Card", job_id, "job_started", job_started)
 		# return job_card
-		job_card = frappe.db.get_value('Job Card', filters, ['job_started','for_quantity','name'])
+		filters = {
+			"name": ["=",job_id],
+		}
+		job_card = frappe.db.get_value('Job Card', filters, ['job_started','for_quantity'])
 		return {
 			'status' : job_card[0],
 			'employee_name' : data_employee['employee_name'],
@@ -94,8 +131,8 @@ def set_actual(mesin_id,actual):
 	if job_started == 0:
 		return {'status':'job card hasn\'t started yet'}
 
-	job_card_time = frappe.db.get_value('Job Card Time Log', filters, '*')
-	return job_card_time
+	job_card_time = frappe.db.get_value('Job Card Time Log', filters, '*', as_dict=True, order_by='idx desc')
+	# return job_card_time
 	if job_card_time:
 		frappe.db.set_value("Job Card Time Log", job_card_time['name'], "actual", actual)
 		return {'status' : 1}
