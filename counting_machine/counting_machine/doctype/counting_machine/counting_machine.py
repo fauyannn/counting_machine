1682c8c0a1a97744ed7b3fc6e107795e40f2199c
Fauyan <fauyannn@gmail.com>
2020-02-26 19:40:20 +0700

    api get_all_data

--- counting_machine/counting_machine/doctype/counting_machine/counting_machine.py
+++ counting_machine/counting_machine/doctype/counting_machine/counting_machine.py
@@ -464,3 +464,15 @@ def get_time_cycle2(bom_no,operation,workstation):
 def get_purchase_order(docstatus):
 	data = frappe.get_list('Purchase Order',filters={'docstatus': docstatus},fields=['*'])
 	return data
+
+@frappe.whitelist()
+def get_all_data(doctype,start,page_length,fields,order_by):
+	data = frappe.get_all(doctype, 
+		start=start, 
+		page_length=page_length, 
+		fields=fields, # "name, hub_category"
+		order_by= order_by  # 'creation desc'
+		)
+	total_data = frappe.db.count(doctype)
+
+	return {'data':data,'total_data':total_data}
